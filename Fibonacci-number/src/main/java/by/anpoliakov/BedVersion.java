05c1b9bc493e178364634d35f530a437e8e4d3f6
anpoliakov <Andrewlybimenko@gmail.com>
2021-03-17 11:50:39 +0300

    bed version fibonacci

--- Fibonacci-number/src/main/java/by/anpoliakov/BedVersion.java
+++ Fibonacci-number/src/main/java/by/anpoliakov/BedVersion.java
@@ -2,6 +2,15 @@ package by.anpoliakov;
 
 public class BedVersion {
     public static void main(String[] args) {
-        
+        System.out.println(bedFib(5));
+    }
+
+    //Плохая реализация, код долго выполняется в рекурсивном цикле (N = 100 -> около 50000 лет)
+    //Очень много дублирующегося кода
+    private static long bedFib(int n){
+        if(n <= 1)
+            return n;
+
+        return bedFib(n - 1) + bedFib(n - 2);
     }
 }
