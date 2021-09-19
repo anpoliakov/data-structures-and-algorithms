18ad21d9cc45cc4b1afcc3675dbc6b4e40907d23
anpoliakov <Andrewlybimenko@gmail.com>
2021-09-19 11:57:07 +0300

    Rename Collection_Framework

---
+++ Collection-Framework/src/an/poliakov/set/Runner.java
@@ -0,0 +1,10 @@
+package an.poliakov.set;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Runner {
+    public static void main(String[] args) {
+        Set<Integer> set = new LinkedHashSet<>();
+    }
+}
