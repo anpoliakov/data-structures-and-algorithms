cb5885903d37e63c038636473eba16eae456ec78
anpoliakov <Andrewlybimenko@gmail.com>
2021-09-22 22:25:42 +0300

    added shaker sort, bubble sort option

--- Bubble-sort/src/by/andrew/bubbleSort/Runner.java
+++ Bubble-sort/src/by/andrew/bubbleSort/Runner.java
@@ -3,8 +3,9 @@ package by.andrew.bubbleSort;
 public class Runner {
 
     public static void main(String[] args) {
-        int [] mas = new int[]{4,1,3,5,2,0,9,6,7,8};
-        startBubbleSort(mas);
+        int [] mas = new int[]{4,1,3,5,2,0,9,6,7,8,1,4,7};
+        //startBubbleSort(mas);
+        startShakerSort(mas);
     }
 
     // стандартная сортировка пузырьком
@@ -18,6 +19,36 @@ public class Runner {
                 }
             }
         }
+        showMas(mas);
+    }
+
+    //шейкерная сортировка (более быстрый аналог пузырьковой)
+    public static void startShakerSort(int [] mas){
+        int leftIndex = 0;
+        int rightIndex = mas.length - 1;
+
+        while (leftIndex <= rightIndex){
+            //тяжёлые элементы в конец
+            for(int i = leftIndex; i < rightIndex; i++){
+                if(mas[i] > mas[i + 1]){
+                    int temp = mas[i];
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = temp;
+                }
+            }
+            rightIndex --;
+
+            //лёгкие элементы в начало
+            for(int j = rightIndex; j > leftIndex; j--){
+                if(mas[j] < mas[j - 1]){
+                    int temp = mas[j];
+                    mas[j] = mas[j - 1];
+                    mas[j - 1] = temp;
+                }
+            }
+            leftIndex ++;
+        }
+
         showMas(mas);
     }
 
