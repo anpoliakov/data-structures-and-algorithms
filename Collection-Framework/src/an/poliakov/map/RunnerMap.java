dd77714d1906b73de4900b2680667bf457da6f09
anpoliakov <Andrewlybimenko@gmail.com>
2021-09-27 22:35:54 +0300

    repeating collections

---
+++ Collection-Framework/src/an/poliakov/map/RunnerMap.java
@@ -0,0 +1,10 @@
+package an.poliakov.map;
+
+/**
+ * Основные операции при работе с map коллекцией
+ * */
+public class RunnerMap {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Collection-Framework/src/an/poliakov/set/RunnerSet.java
@@ -0,0 +1,12 @@
+package an.poliakov.set;
+
+/**
+ *
+ * Основные операции и приёмы работы с Set коллекцией
+ * 
+ * */
+public class Runner {
+    public static void main(String[] args) {
+
+    }
+}
