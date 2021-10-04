05a2b36c2780863e5de82f2f3c272600e34f40ea
anpoliakov <Andrewlybimenko@gmail.com>
2021-10-04 22:25:25 +0300

    structure SetofStacks

--- Collection-Framework/src/an/poliakov/queue/RunnerQueue.java
+++ Collection-Framework/src/an/poliakov/queue/RunnerQueue.java
@@ -6,6 +6,8 @@ import java.util.Queue;
 /**
  *  Основные операции при работе с очередью
  * */
+
+//TODO на 05.10 - структура данных SetofStacks которая при заполнение стека создаёт новый и заполнение продолжается в новый стек
 public class RunnerQueue {
     public static void main(String[] args) {
         Queue <Integer> queue = new PriorityQueue<>();
---
+++ Collection-Framework/src/an/poliakov/queue/setofstacks/SetofStacks.java
@@ -0,0 +1,20 @@
+package an.poliakov.queue.setofstacks;
+
+import java.util.ArrayList;
+
+/**
+ * Основной класс который будет заниматься всем распределением обьектов по стекам
+ * */
+public class SetofStacks {
+    ArrayList <String> stacks = new ArrayList<>();
+
+    //универсальнеый метод для добавления
+    public void push (String string){
+
+    }
+
+    //универсальнеый метод для получения
+    public String pop (){
+        return null;
+    }
+}
