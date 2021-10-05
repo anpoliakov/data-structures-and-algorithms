59f72811146c86ad39f42c210a51589e9c891e63
anpoliakov <Andrewlybimenko@gmail.com>
2021-10-06 00:01:22 +0300

    method for add new data

--- Collection-Framework/src/an/poliakov/queue/RunnerQueue.java
+++ Collection-Framework/src/an/poliakov/queue/RunnerQueue.java
@@ -1,7 +1,6 @@
 package an.poliakov.queue;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 /**
  *  Основные операции при работе с очередью
@@ -10,6 +9,16 @@ import java.util.Queue;
 //TODO на 05.10 - структура данных SetofStacks которая при заполнение стека создаёт новый и заполнение продолжается в новый стек
 public class RunnerQueue {
     public static void main(String[] args) {
-        Queue <Integer> queue = new PriorityQueue<>();
+        ArrayDeque <Integer> queue = new ArrayDeque<>();
+        Stack<Integer> steck = new Stack<>();
+        steck.add(133);
+        steck.add(144);
+        steck.add(155);
+        Integer peek = steck.search(144);
+        System.out.println("ID = " + peek);
+        System.out.println(steck);
+
+        LinkedList <Integer> list = new LinkedList<>();
+
     }
 }
--- Collection-Framework/src/an/poliakov/queue/setofstacks/SetofStacks.java
+++ Collection-Framework/src/an/poliakov/queue/setofstacks/SetofStacks.java
@@ -1,16 +1,42 @@
 package an.poliakov.queue.setofstacks;
 
 import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 /**
  * Основной класс который будет заниматься всем распределением обьектов по стекам
  * */
 public class SetofStacks {
-    ArrayList <String> stacks = new ArrayList<>();
+    private final int MAX_SIZE_STECK = 5;
+    private int copacity;
+
+    private ArrayList <PriorityQueue<String>> stacks = new ArrayList<>();
+
+    public SetofStacks(int copacity){
+        this.copacity = copacity;
+    }
+
+    //если list очередей не пуст - возвращаем последнюю Queue
+    private PriorityQueue<String> getLastQeque(){
+        if(stacks.size() == 0){
+            return null;
+        }
+        return stacks.get(stacks.size() - 1);
+    }
 
     //универсальнеый метод для добавления
     public void push (String string){
-
+        PriorityQueue <String> lastQueue = getLastQeque();
+        if(lastQueue != null && lastQueue.size() < MAX_SIZE_STECK){
+            lastQueue.add(string);
+        }else{
+            //значит нет ниодной очереди или последняя очередь заполнена
+            //следовательно создаём
+            PriorityQueue <String> priorityQueue = new PriorityQueue<>();
+            priorityQueue.add(string);
+            stacks.add(priorityQueue);
+        }
     }
 
     //универсальнеый метод для получения
